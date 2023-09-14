14fbe9937029345b8879ae6d851f4aa35a80155e
Fannn03 <fann.fann771@gmail.com>
2023-09-14 22:06:21 +0700

    build(schema): add req reimbursment

---
+++ prisma/migrations/20230914150302_create_table_request_reimburses/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `request_reimburses` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` INTEGER NOT NULL,
+    `description` LONGTEXT NOT NULL,
+    `type` ENUM('transportation', 'dentist', 'glassess', 'others') NOT NULL,
+    `nominal` BIGINT NOT NULL,
+    `url_proof` VARCHAR(191) NULL,
+    `status` ENUM('pending', 'approved_lead', 'approved_hr', 'reject') NOT NULL DEFAULT 'pending',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+    `deleted_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
