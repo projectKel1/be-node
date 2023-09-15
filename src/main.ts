5d89b473b3f3ab84fd3fc40afc9833d600d3b144
Fannn03 <fann.fann771@gmail.com>
2023-09-15 09:54:11 +0700

    refactor(reimburstment): change format filename

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express'
 import 'dotenv/config'
-import RequestReimburmentRouter from './routes/req-reimbursment-router'
+import ReimburmentRouter from './routes/reimbursment-router'
 
 const app: express.Application = express()
 const env = process.env
@@ -8,7 +8,7 @@ const env = process.env
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use('/request-reimbursment', RequestReimburmentRouter)
+app.use('/request-reimbursment', ReimburmentRouter)
 
 app.listen(env.SERVER_PORT, () => {
     console.log(`Listening on port ${env.SERVER_PORT}`)
--- src/routes/reimbursment-router.ts
+++ src/routes/reimbursment-router.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createData, getAllData } from '../controllers/req-reimbursment-controller'
+import { createData, getAllData } from '../controllers/reimbursment-controller'
 import reimbursmentMiddleware from '../middleware/reimbursment-middleware'
 
 const router = express.Router()
