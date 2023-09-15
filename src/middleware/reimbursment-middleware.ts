9197c77c18ec5e3a7fddc5b4b8c5ee47af887779
Fannn03 <fann.fann771@gmail.com>
2023-09-15 09:44:14 +0700

    feat(reimbursment): using joi to handle validation req body

---
+++ src/middleware/reimbursment-middleware.ts
@@ -0,0 +1,68 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+
+export default async (req: Request, res: Response, next: NextFunction) => {
+
+    const validation = Joi.object({
+        description: Joi.string()
+            .required()
+            .empty()
+            .min(5)
+            .messages({
+                'any.required': 'description field is required',
+                'string.empty': 'description field cannot has empty value',
+                'string.min': 'description field cannot less than 5 characters'
+            })
+        ,
+        type: Joi.string()
+            .required()
+            .empty()
+            .valid('transportation', 'dentist', 'glasses', 'others')
+            .messages({
+                'any.required': 'type field is required',
+                'string.empty': 'type field cannot has empty value'
+            })
+        ,
+        nominal: Joi.number()
+            .required()
+            .empty()
+            .min(500)
+            .unsafe()
+            .messages({
+                'any.required': 'type nominal is required',
+                'string.empty': 'type nominal cannot has empty value',
+                'number.base': 'type nominal must be a number',
+                'number.min': 'type nominal cannot less than 500'
+            })
+        ,
+        url_proof: Joi.string()
+            .required()
+            .empty()
+            .messages({
+                'any.required': 'url_proof field is required',
+                'string.empty': 'url_proof field cannot has empty value',
+            })
+    })
+
+    try {
+        await validation.validateAsync(req.body, {
+            abortEarly: false
+        })
+    } catch (err: any) {
+
+        let errorMessages: any = {}
+        err.details.map((err: any) => {
+            errorMessages[err.context.key] = err.message
+        })
+
+        return res.status(400).json({
+            status_code: 400,
+            result: 'error',
+            message: errorMessages
+        })
+
+    }
+
+    return next()
+
+}
