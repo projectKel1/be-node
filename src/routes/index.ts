907f3bf9904aa5cb9c08b02073d08662b6d52a60
Fannn03 <fann.fann771@gmail.com>
2023-09-20 16:38:40 +0700

    feat(target): add level middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,12 +4,13 @@ import RequestLeaves from './leaves-router'
 import Attendances from './attendances-router'
 import Targets from './targets-router'
 import AuthMiddleware from '../middleware/auth-middleware'
+import LevelMiddleware from '../middleware/level-middleware'
 
 const router = express.Router()
 
 router.use('/request-leaves', AuthMiddleware, RequestLeaves)
 router.use('/request-reimbursement', AuthMiddleware, RequestReimbursement)
 router.use('/attendances', AuthMiddleware, Attendances)
-router.use('/targets', AuthMiddleware, Targets)
+router.use('/targets', AuthMiddleware, LevelMiddleware("manager"), Targets)
 
 export default router
