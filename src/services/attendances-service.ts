64421a4e5f3da86ab56aaeab10b718b82f2d0416
Fannn03 <fann.fann771@gmail.com>
2023-09-21 10:09:31 +0700

    perf: show data based on their roles

--- src/services/attendances-service.ts
+++ src/services/attendances-service.ts
@@ -1,8 +1,12 @@
 import { PrismaClient, Prisma } from "@prisma/client"
+import { Request } from "express"
+import { UserData } from "./reimbursement-service"
+import { getAllUsers } from "../api/users"
 
 interface Attendances {
     id: number,
     user_id: number,
+    fullname?: string | undefined,
     is_checkout: boolean,
     created_at: Date,
     updated_at: Date,
@@ -26,21 +30,62 @@ const prisma = new PrismaClient().$extends({
     }
 })
 
-export const getDataAttendances = async (query: Attendances, skip: number, take: number) => {
+export const getDataAttendances = async (req: Request, skip: number, take: number) => {
 
-    let data: Attendances[]
-    
-    try {
-        data = await prisma.attendance.findMany({
-            where: query,
-            skip: skip,
-            take: take
+    let attendances: Attendances[]
+    let user: UserData[] | null
+    let json: any = []
+    let query: any = req.query
+
+    if(req.user.level == "EMPLOYEE") {
+
+        query.user_id = req.user.userId
+        try {
+            attendances = await prisma.attendance.findMany({
+                where: query,
+                skip: skip,
+                take: take
+            })
+        } catch (err) {
+            throw new Error("invalid params query")
+        }
+
+        return attendances
+
+    } else {
+
+        user = await getAllUsers(req, req.user.userId)
+
+        try {
+            attendances = await prisma.attendance.findMany({
+                where: req.query,
+                skip: skip,
+                take: take
+            })
+        } catch (err) {
+            throw new Error("invalid params query")
+        }
+
+        if(!attendances) return attendances
+
+        attendances.map((value: Attendances) => {
+            let username = user?.find((user: UserData) => user.id === value.user_id)
+
+            let data = {
+                id: value.id,
+                user_id: value.user_id,
+                fullname: username,
+                is_checkout: value.is_checkout,
+                created_at: value.created_at,
+                updated_at: value.updated_at,
+            }
+
+            json.push(data)
         })
-    } catch (err) {
-        throw new Error("Invalid params query")
-    }
 
-    return data
+        return json.filter((value: any) => value.fullname !== undefined)
+
+    }
 
 }
 
