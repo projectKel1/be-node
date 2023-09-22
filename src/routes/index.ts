340d1f3aceb5def37db9c1fc92077c1df4b6775f
Fannn03 <fann.fann771@gmail.com>
2023-09-22 14:26:42 +0700

    fix(reports): force delete due missing property in prisma schema

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import RequestReimbursement from './reimbursement-router'
 import RequestLeaves from './leaves-router'
 import Attendances from './attendances-router'
 import Targets from './targets-router'
-import ReportTarget from './targets-router'
 import AuthMiddleware from '../middleware/auth-middleware'
 import LevelMiddleware from '../middleware/level-middleware'
 
@@ -15,7 +14,6 @@ router.use('/request-leaves', AuthMiddleware, RequestLeaves)
 router.use('/request-reimbursement', AuthMiddleware, RequestReimbursement)
 router.use('/attendances', AuthMiddleware, Attendances)
 router.use('/targets', AuthMiddleware, LevelMiddleware(["manager", "c-level"]), Targets)
-router.use('/report-targets', AuthMiddleware, LevelMiddleware("manager"), ReportTarget)
 
 // Manager
 router.use('/managers', AuthMiddleware, LevelMiddleware(["manager", "c-level"]), ManagerRoute)
